3d600b93d973161864bdc8673f05d2b6c84031f6
Quentin <quentino.vega@gmail.com>
2021-07-26 09:59:18 +0200

    user can add multiple test or when constraints

--- src/utils/resolvers/baseResolver.ts
+++ src/utils/resolvers/baseResolver.ts
@@ -1,7 +1,7 @@
 import * as Constraints from './types';
 import * as yup from 'yup'
 
-import {buildSubResolver} from '../form';
+import { buildSubResolver } from '../form';
 
 export type BaseResolverConstraints = {
   required?: Constraints.SimpleConstraint;
@@ -13,7 +13,7 @@ export type BaseResolverConstraints = {
 export class BaseResolver {
   constructor(type: string, constraints: BaseResolverConstraints) {
     this.type = type
-    
+
     this.required = constraints.required
     this.test = constraints.test
     this.when = constraints.when
@@ -23,7 +23,7 @@ export class BaseResolver {
 
   required?: Constraints.SimpleConstraint;
   test?: Constraints.TestConstraint | Array<Constraints.TestConstraint>;
-  when?: Constraints.WhenConstraint | Array<Constraints.WhenConstraint>;
+  when?: Constraints.WhenConstraint | Constraints.WhenConstraint[];
   oneOf?: Constraints.OneOfConstraint;
 
   toBaseResolver(yupResolver: yup.AnySchema, key: string, dependencies: any) {
@@ -39,7 +39,7 @@ export class BaseResolver {
         })
       } else {
         baseResolver = baseResolver.test(String(this.test.ref), this.test.message, this.test.test)
-        dependencies.push([key , this.test.ref])
+        dependencies.push([key, this.test.ref])
       }
     }
 
@@ -48,7 +48,6 @@ export class BaseResolver {
     }
 
     if (!!this.when) {
-
       if (Array.isArray(this.when)) {
         this.when.forEach(when => {
           baseResolver = baseResolver.when([String(when.ref)], {
@@ -60,16 +59,14 @@ export class BaseResolver {
         })
       } else {
         baseResolver = baseResolver.when([String(this.when.ref)], {
-          is: (val: any) => this.when?.test(val), //FIXME
-          then: buildSubResolver({type: this.type, constraints: this.when.then}, key, dependencies),
+          is: (val: any) => !Array.isArray(this.when) && this.when?.test(val),
+          then: buildSubResolver({ type: this.type, constraints: this.when.then }, key, dependencies),
           otherwise: buildSubResolver({ type: this.type, constraints: this.when.otherwise }, key, dependencies)
         })
         dependencies.push([key, this.when.ref])
       }
-
-
     }
-    
+
     return baseResolver
   }
 }
